cc2dc01206bb9895786349da533aa27963575828
myfor <mrmyfor@outlook.com>
2020-01-18 17:53:32 +0800

    add: clients

---
+++ src/Domain/Clients/Models.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Clients
+{
+    public class Models
+    {
+        public struct UserIntro
+        {
+            public int Id { get; set; }
+            public string Account { get; set; }
+            public string Avatar { get; set; }
+        }
+    }
+}
