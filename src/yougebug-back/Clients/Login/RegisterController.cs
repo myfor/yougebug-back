77301f3cc78f52767bd173af20c77f80d5aea3cd
myfor <mrmyfor@outlook.com>
2020-02-25 16:34:42 +0800

    update: register controller

--- src/yougebug-back/Clients/Login/RegisterController.cs
+++ src/yougebug-back/Clients/Login/RegisterController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using yougebug_back.Shared;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace yougebug_back.Clients.Login
 {
@@ -14,36 +10,19 @@ namespace yougebug_back.Clients.Login
     [AllowAnonymous]
     public class RegisterController : ClientBaseController
     {
+        /*
+        注册一个用户账号
+         */
         // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<IActionResult> Index(Domain.Clients.Models.RegisterInfo register)
         {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            (bool isValid, string msg) = register.IsValid();
+            if (!isValid)
+                return Pack(Resp.Fault(Resp.NONE, msg));
+            Domain.Clients.Hub clientHub = new Domain.Clients.Hub();
+            Resp r = await clientHub.Register(register);
+            return Pack(Resp.Success(r));
         }
     }
 }
