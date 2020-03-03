2e2fc4ff89819e543a48a6c35b306c00534a6431
myfor <mrmyfor@outlook.com>
2020-03-03 14:36:12 +0800

    add: search page

--- src/yougebug-back/Controllers/Questions/QuestionsController.cs
+++ src/yougebug-back/Controllers/Questions/QuestionsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using yougebug_back.ViewModels.Questions;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,15 +13,36 @@ namespace yougebug_back.Controllers.Questions
     public class QuestionsController : Controller
     {
         /*
-        问题列表
+        最新问题列表
          */
         // GET: /<controller>/
         [HttpGet]
-        public IActionResult List(string search)
+        public IActionResult Newest()
         {
-            ViewBag.Title = string.IsNullOrWhiteSpace(search) ? Shared.Defaults.PAGE_DEFAULT_TITLE : "有个bug，提问-" + search;
+            ViewBag.Title =  "有个bug，最新提问";
 
-            return View("List");
+            return View("Newest");
+        }
+
+        /*
+        搜索
+         */
+        [HttpGet("search/{s}")]
+        public IActionResult SearchResult(string s, int index, int size)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return Newest();
+
+            ViewBag.Title = "有个bug，提问 - " + s;
+
+            Domain.Paginator page = Domain.Paginator.New(index, size);
+            page.TotalRows = 100;
+
+            SearchModel model = new SearchModel();
+            model.Search = s;
+            model.Page = page;
+
+            return View("Search", model);
         }
     }
 }
---
+++ src/yougebug-back/ViewModels/Questions/SearchModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yougebug_back.ViewModels.Questions
+{
+    public class SearchModel
+    {
+        public string Search { get; set; }
+        public Domain.Paginator Page { get; set; }
+    }
+}
