42dbaffb6c3cc402f17efe8270745e713a05945e
myfor <mrmyfor@outlook.com>
2020-03-04 18:07:14 +0800

    update: register

--- src/yougebug-back/wwwroot/js/common.js
+++ src/yougebug-back/wwwroot/js/common.js
@@ -8,3 +8,10 @@ function getQueryString(name) {
     }
     return '';
 }
+
+//  验证邮箱格式
+function checkEmail(email) {
+    email = email.trim();
+    let reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
+    return reg.test(email);
+}
