733f2da19b233b9f46c6573c59d6c6a4fbb3ab73
mrunowen <birenchens@outlook.com>
2020-04-06 13:44:35 +0800

    fix: bug

--- src/Domain/Questions/List/FromUserSelf.cs
+++ src/Domain/Questions/List/FromUserSelf.cs
@@ -22,11 +22,11 @@ namespace Domain.Questions.List
 
         public async Task<Resp> GetListAsync(Paginator pager)
         {
-            Expression<Func<DB.Tables.Question, bool>> whereStatement = q => q.State != (int)Question.QuestionState.Remove;
+            Expression<Func<DB.Tables.Question, bool>> whereStatement;
 
             //  要获取的人的ID
             if (int.TryParse(pager.Params["userId"] ?? "", out int userId))
-                whereStatement = whereStatement.And(q => q.AskerId == userId);
+                whereStatement = q => q.AskerId == userId;
             else
                 return Resp.Fault(Resp.NONE, "");
 
